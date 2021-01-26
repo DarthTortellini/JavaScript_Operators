f8e1bd63166607ad89b0c8e182676b12fb03e441
Eli Lusk <egle09.lusk@gmail.com>
2021-01-26 14:18:55 -0600

    hoy

--- app.js
+++ app.js
@@ -4,16 +4,63 @@ console.log(
 );
 
 // Exercise 1
+
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
-
+var a=20
+var b=4 
+var add=a+b
+var minus=a-b
+var multiply=a*b
+var divide=a/b
+console.log(`${a}
+${b}
+${add}
+${minus}
+${multiply}
+${divide}
+`)
 // Exercise 2
+
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
 
+let answer1= c + str
+let answer2= c + str2
+let answer3=c + isPresent
+let answer4=c + firstName
+let answer5=c + lastName
+
+console.log(answer1)
+console.log(answer2)
+console.log(answer3)
+console.log(answer4)
+console.log(answer5)
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d==str3)
+console.log(d===str3)
+console.log(!isPresent2)
+console.log(!isPresent2 || isPresent2)
+console.log("eleven"==str4 && d >=str3)
+console.log(0 == false)
+console.log(0===false)
+console.log(0 != false)
+console.log(0 !== false)
